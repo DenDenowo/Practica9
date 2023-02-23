ff0c470ddef077a2041d79beac1f3f705bea0e5f
DenDenowo <paugumi23@gmail.com>
2023-02-22 18:19:13 -0600

    Solicitar atributos en main

--- Main.py
+++ Main.py
@@ -1,16 +1,31 @@
 #1. Importar la clase
 from Personaje import *
 
-#2. Instanciar un objeto
+#2. Solicitar atributos para el objeto
+print("")
+print("### Solicitud de datos Heroe ###")
+espH = input("Escribe la especie del héroe: ")
+nomH = input("Escribe el nombre del héroe: ")
+altH = float(input("Escribe la altura del héroe: "))
+cargaH = int(input("¿Cuántas balas se puede recargar?: "))
+
+print("")
+print("### Solicitud de datos Villano ###")
+espV = input("Escribe la especie del villano: ")
+nomV = input("Escribe el nombre del villano: ")
+altV = float(input("Escribe la altura del villano: "))
+cargaV = int(input("¿Cuántas balas se puede recargar?: "))
+
+#3. Instanciar un objeto
 Heroe = Personaje()
 
-#3. Acceder a sus atributos
+#4. Acceder a sus atributos
 
 Heroe.nombre 
 Heroe.especie 
 Heroe.altura 
 
-#4. Acceder a sus metodos
+#5. Acceder a sus metodos
 
         
 print('Atributos del personaje: ')
